9cedb9d5ee3dcae19a3bb58e0d7bf324e9310abd
15yonnets <15yonnets@D22-P01-W7.estransup.local>
2017-03-27 16:14:52 +0200

    sdf

---
+++ src/model.php
@@ -0,0 +1,10 @@
+<?php
+
+function getAllSejours(){
+	$bdd = new PDO('mysql:host=localhost;dbname=bdstpaul;charset=utf8', 'root');
+	$sejours = $bdd->query('select * from sejour order by sejno');
+	return $sejours;
+}
+
+
+?>
---
+++ web/index.php
@@ -0,0 +1,18 @@
+<?php /*
+	include "model.php";
+	$sejours = getSejours();
+	include "view.php";
+    */
+
+
+
+
+    require_once __DIR__.'/../vendor/autoload.php';
+
+    $app = new Silex\Application();
+
+    require __DIR__.'/../app/routes.php';
+
+    $app->run();
+
+ ?>
